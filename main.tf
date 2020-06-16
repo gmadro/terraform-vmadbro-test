1652fa2727c1a094c6a9293bf3e055394391eb72
Greg Madro <greg.madro@gmail.com>
2020-06-15 22:43:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+    backend "s3" {
+        bucket = "vmadbro-terraform-state"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform-state"
+    }
+}
+
 resource "aws_instance" "remote"{
     ami = "ami-09d95fab7fff3776c"
     instance_type = "t2.micro"
